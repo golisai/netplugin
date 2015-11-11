8473cea3719f41e3b39a5dd2667fb941548cdd65
Sri Goli <srgoli@cisco.com>
2015-11-10 18:57:41 -0800

    Read containers from docker daemon

--- aci/agent/agent.go
+++ aci/agent/agent.go
@@ -19,34 +19,65 @@ import (
 	"flag"
 	"github.com/contiv/netplugin/aci"
 	"github.com/contiv/netplugin/aci/model/comp"
+	"github.com/samalba/dockerclient"
 	"log"
+	"os"
+	"strings"
 )
 
 const Provider = "VMware"
 
+func addContainer(clt *aci.Client, info *dockerclient.ContainerInfo) error {
+	log.Println("Adding container: ", info.Name, info.NetworkSettings.IPAddress)
+	// Create Ctrlr
+	ctrlr := comp.NewCtrlr(Provider, "contiv", "contiv")
+	
+	// Add HV
+	hostName, _ := os.Hostname()
+	hv := ctrlr.AddHv(hostName)
+	hv.AddHpNic("eth0")
+
+	name := strings.TrimPrefix(info.Name, "/")
+	
+	vm := ctrlr.AddVm(hv, name)
+	vnic := vm.AddVNic(info.Config.MacAddress)
+	vnic.AddIp(info.NetworkSettings.IPAddress)
+
+	return clt.Write(ctrlr)
+}
+
 func main() {
-	url := flag.String("aci.url", "http://srgoli-bld:8000", "APIC Rest URL")
-	user := flag.String("aci.user", "admin", "APIC login username")
-	passwd := flag.String("aci.passwd", "ins3965!", "APIC login passwd")
+	url := flag.String("url", "http://srgoli-bld:8000", "APIC Rest URL")
+	user := flag.String("user", "admin", "APIC login username")
+	passwd := flag.String("passwd", "ins3965!", "APIC login passwd")
 
 	flag.Parse()
 
 	clt, err := aci.NewClient(*url, *user, *passwd)
 	if err != nil {
-		log.Fatal("Cant' create client", err)
+		log.Fatal("Can't create client", err)
 	}
 
-	// Create Ctrlr
-	ctrlr := comp.NewCtrlr(Provider, "contiv", "contiv")
-	// Add HV
-	hv := ctrlr.AddHv("hv")
-	hv.AddHpNic("pnic1")
-	vm := ctrlr.AddVm(hv, "vm1")
-	vnic := vm.AddVNic("AA:AA:AA:AA:AA:AA")
-	vnic.AddIp("1.1.1.1")
-	if err := clt.Write(ctrlr); err != nil {
-		log.Fatal(err)
+	// Init the client
+	docker, err := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
+	if err != nil {
+		log.Fatal("Cant' create docker client", err)
 	}
-
-
+	
+	// Get running containers
+	containers, err := docker.ListContainers(false, false, "")
+	if err != nil {
+		log.Fatal("Can't list containers", err)
+	}
+	
+	for _, c := range containers {
+		info, err := docker.InspectContainer(c.Id)
+		if err != nil {
+			log.Fatal("Inspect failed", err)	
+		}
+		if err = addContainer(clt, info); err != nil {
+			log.Fatal("addContainer failed", err)
+		}
+	}
+	
 }
